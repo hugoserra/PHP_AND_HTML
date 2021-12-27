22e5ffc1c981b1737dc3ea05a410bf05a4643b83
hugoserra <tcarvalhotaichou@gmail.com>
2021-12-27 14:26:53 -0300

    formulario php, html

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$mensagem = $_POST['mensagem'];
+
+echo "Ola $nome, a sua mensagem foi enviada para o email:$email";
+echo "<br>$mensagem";
+
+ ?>
